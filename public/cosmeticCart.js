4cb3b0df18c797c0a30c5f41efea996f2af31fdc
chong lepcha <ringchen>
2025-05-19 11:30:59 +0530

    ringchen

--- public/cosmeticCart.js
+++ public/cosmeticCart.js
@@ -28,7 +28,7 @@ function renderCart() {
     cart.forEach(item=>{
         const div=
         document.createElement('div');
-        div.calssName='cart-item';
+        div.className='cart-item';
         div.innerHTML=`
         <div class="col-md-12">
             <div class="row">
